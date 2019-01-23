a47bec73fc900045c526116bc02ed4e84bcbf128
Tse Jun Zhen <victor@Tses-MacBook-Pro.local>
2019-01-23 16:35:04 +0800

    Fixed:link

--- myJS.js
+++ myJS.js
@@ -13,13 +13,14 @@ function getAnimation(){
       loop: true,
       autoplay: true,
       path: commentUulJson,
+
       });
 }
 
 window.onload=function(){
   $.ajax({
     type: 'GET',
-    url: 'https://app-versions.herokuapp.com/index/index/f8d1d0e0-b1d7-4892-b960-62f907cdeaae',//'https://app-versions.herokuapp.com/index/index/f8d1d0e0-b1d7-4892-b960-62f907cdeaae',//http://52.175.12.176/index/index/a1f6c42d-ab0e-4903-b6e6-72058e899e23',
+    url: 'https://uniquetoone.com/index/index/a1f6c42d-ab0e-4903-b6e6-72058e899e23',//'https://app-versions.herokuapp.com/index/index/f8d1d0e0-b1d7-4892-b960-62f907cdeaae',//http://52.175.12.176/index/index/a1f6c42d-ab0e-4903-b6e6-72058e899e23',
     dataType: 'json',
     // crossDomain: true,
     success: function (response){
