ba43240d0e5bdccd38c1dfad7653a54ef8a44192
Tse Jun Zhen <victor@Tses-MacBook-Pro.local>
2019-01-23 15:00:52 +0800

    Changed:jquery to vue

--- myJS.js
+++ myJS.js
@@ -1,6 +1,9 @@
 //lettering js
 // $(".loading").lettering();
 
+
+
+
 function getAnimation(){
     // animation data.json URL
     let commentUulJson = "https://nininenine.github.io/animation_oneMax/data.json";
@@ -26,7 +29,7 @@ function toChangedElement(response){
 window.onload=function(){
   $.ajax({
     type: 'GET',
-    url: 'https://uniquetoone.com/index/index/b49567a1-05f1-43e5-ad29-e2a191bbdf44',//'https://app-versions.herokuapp.com/index/index/4c59282b-eb9b-4b74-86c7-0fd2d338667b',//http://52.175.12.176/index/index/a1f6c42d-ab0e-4903-b6e6-72058e899e23',
+    url: 'https://uniquetoone.com/index/index/a1f6c42d-ab0e-4903-b6e6-72058e899e23',//'https://app-versions.herokuapp.com/index/index/4c59282b-eb9b-4b74-86c7-0fd2d338667b',//http://52.175.12.176/index/index/a1f6c42d-ab0e-4903-b6e6-72058e899e23',
     dataType: 'json',
     // crossDomain: true,
     success: function (response){
@@ -34,15 +37,55 @@ window.onload=function(){
       var isOpen = response.version;
 
       if (isOpen == 1){
-        getAnimation()
+        // getAnimation()
 
-        toChangedElement(response)
-      }
+        // toChangedElement(response)
+        $.ajax({
+          type: 'GET',
+          url: 'https://app-versions.herokuapp.com/index/index/f8d1d0e0-b1d7-4892-b960-62f907cdeaae',//'https://app-versions.herokuapp.com/index/index/4c59282b-eb9b-4b74-86c7-0fd2d338667b',//http://52.175.12.176/index/index/a1f6c42d-ab0e-4903-b6e6-72058e899e23',
+          dataType: 'json',
+          // crossDomain: true,
+          success: function (response){
+            var isOpen = response.version;
+            if (isOpen == 1){
+              getAnimation()
+              // toChangedElement(response)
+            } else if (isOpen == 2){
 
-      if (isOpen == 2){
+              toChangedElement(response)
+              var newUrl = response.msg.new_url;
 
-        toChangedElement(response)
+              var bm = new Vue({
+                el:'#openKey',
+                data:{
+                  message: response
+                }
+              });
+
+              getAnimation()
+              console.log(response);
+              console.log(response.msg.is_active);
+              console.log(isOpen);
+              console.log(newUrl);
+              setTimeout(function() { window.location = newUrl; }, 5000);
+            }
+          },
+
+          error:function(xhr){
+          alert("發生錯誤: " + xhr.status + " " + xhr.statusText);
+          }
+        });
+      } else if (isOpen == 2){
+
+        // toChangedElement(response)
         var newUrl = response.msg.new_url;
+
+        var bm = new Vue({
+          el:'#openKey',
+          data:{
+            message: response
+          }
+        });
 
         getAnimation()
 
