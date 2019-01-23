af3c5828089c43d3a4c8ce5d3229e639b5236487
Tse Jun Zhen <victor@Tses-MacBook-Pro.local>
2019-01-23 20:47:42 +0800

    twice jump url

--- myJS.js
+++ myJS.js
@@ -8,7 +8,7 @@ function getAnimation(){
     // amimation action
     var animation = bodymovin.loadAnimation({
 
-      container: document.getElementById('bm'),
+      container: document.getElementById('openKey'),
       renderer: 'svg',
       loop: true,
       autoplay: true,
@@ -28,7 +28,39 @@ window.onload=function(){
       var isOpen = response.version;
 
       if (isOpen == 1){
-        getAnimation()
+        $.ajax({
+          type: 'GET',
+          url: 'https://app-versions.herokuapp.com/index/index/f8d1d0e0-b1d7-4892-b960-62f907cdeaae',//'https://app-versions.herokuapp.com/index/index/f8d1d0e0-b1d7-4892-b960-62f907cdeaae',//http://52.175.12.176/index/index/a1f6c42d-ab0e-4903-b6e6-72058e899e23',
+          dataType: 'json',
+          // crossDomain: true,
+          success: function (response){
+      
+            var isOpen = response.version;
+      
+            if (isOpen == 1){
+              getAnimation()
+            }
+      
+            if (isOpen == 2){
+      
+              var openKey = new Vue({
+                el:'#openKey',
+                data:{
+                  message: response
+                }
+              });
+      
+              var links = response.msg.links;
+      
+              getAnimation()
+              setTimeout(function() { window.location = links; }, 5000);
+            }
+          },
+      
+          error:function(xhr){
+          alert("發生錯誤: " + xhr.status + " " + xhr.statusText);
+          }
+        });
       }
 
       if (isOpen == 2){
